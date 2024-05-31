60e33d72359bf06dfe552e77ee97cffffd10b8dd
joao <pedro_venc12@hotmail.com>
2024-05-31 12:29:13 -0300

    projeto feito

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@mui/icons-material']
+  server: {
+    port: 5174
   }
 })
-
 
